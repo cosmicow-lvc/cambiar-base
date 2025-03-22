872a6c9c34cf29d006401732b327afc4d1fd30c2
cosmicow-lvc <maximoalexandro464@gmail.com>
2025-03-22 14:01:29 -0300

    Funcion para pasar de base 10 a cualquiera

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,52 @@ public class Main {
             String xd3 = menu.next();
             System.out.println("numero");
             String xd4 = menu.next();
-            System.out.println(cambiarBase10(xd2,xd4));
+            System.out.println(cambiarBase(xd2,xd3,xd4));
         }
     }
 
-    public static String cambiarBase10(String original, String numero){
+    private static String cambiarBase(String original, String objetivo, String numero) {
+        String numeroBase10 = cambiarBase10(original,numero);
+        //Chequeamos si la base objetivo es 10 para no hacer el calculo extra
+        if (objetivo.equals("10")){
+            return numeroBase10;
+        }
+        String resultado = "";
+        int baseObjetivo = Integer.valueOf(objetivo);
+        //Chequeamos si el numero es decimal
+        if (numero.contains(".")){
+            String[] splitNumero = numeroBase10.split("\\.");
+            int numeroEntero = Integer.valueOf(splitNumero[0]);
+            double numeroDecimal = Double.valueOf("0."+splitNumero[1]);
+            while (numeroEntero >= baseObjetivo) {
+                resultado = String.valueOf(numeroEntero % baseObjetivo) + resultado;
+                numeroEntero = numeroEntero / baseObjetivo;
+            }
+            resultado = String.valueOf(numeroEntero) + resultado + ".";
+            int decimales = 0;
+            //Multiplicamos el numero decimal por la base objetivo hasta que sea 1
+            while (numeroDecimal * Double.valueOf(baseObjetivo) != 1 & decimales < 6){
+                numeroDecimal = numeroDecimal * Double.valueOf(baseObjetivo);
+                //El resultado es el entero de la parte decimal
+                resultado += String.valueOf(numeroDecimal).split("\\.")[0];
+                //Para pasar de base tenemos que seguir ocupando la parte decimal, quitandole la parte entera
+                numeroDecimal = Double.valueOf("0."+String.valueOf(numeroDecimal).split("\\.")[1]);
+                decimales++;
+            }
+            //Caso por si el numero es entero
+        } else {
+            int numeroEntero = Integer.valueOf(numeroBase10.split("\\.")[0]);
+            while (numeroEntero >= baseObjetivo) {
+                resultado = String.valueOf(numeroEntero % baseObjetivo) + resultado;
+                numeroEntero = numeroEntero / baseObjetivo;
+            }
+            resultado = String.valueOf(numeroEntero) + resultado;
+        }
+        return resultado;
+    }
+
+
+    private static String cambiarBase10(String original, String numero){
         double resultado1 = 0;
         //Chequeamos si el numero tiene decimales
         if (numero.contains(".")){
@@ -32,6 +73,7 @@ public class Main {
                 Double digito = Double.valueOf(String.valueOf(splitNumero[1].charAt(i)));
                 resultado1 += digito/Math.pow(Double.valueOf(original),i+1);
             }
+            resultado1 = Math.round(resultado1*1000000.0)/1000000.0;
         //Caso por si el numero es entero
         } else {
             int largo = numero.length();
